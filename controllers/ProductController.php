1f58d91e7bde035d7a41bfee78ddfcd90701e2e1
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2024-07-27 18:12:53 +0000

    Fetched products from database

---
+++ controllers/ProductController.php
@@ -0,0 +1,41 @@
+<?php
+require_once '../models/Product.php';
+
+class ProductController
+{
+    private $productModel;
+
+    public function __construct()
+    {
+        $this->productModel = new Product();
+    }
+
+    public function getAllProducts()
+    {
+        $products = $this->productModel->getAllProducts();
+
+        if ($products) {
+            $results = [];
+            foreach ($products as $product) {
+                $results[] = [
+                    'id' => $product['product_id'],
+                    'categoryId' => $product['category_id'],
+                    'name' => $product['product_name'],
+                    'desc' => $product['description'],
+                    'img' => $product['product_image'],
+                    'price' => $product['price'],
+                    'stock' => $product['stock_quantity'],
+                ];
+            }
+
+            // echo json_encode($results);
+            return $results;
+        }
+    }
+
+
+    public function getSingleProduct($id)
+    {
+        return $this->productModel->getSingleProduct($id);
+    }
+}
---
+++ models/Product.php
@@ -0,0 +1,36 @@
+<?php
+require_once '../config/Dbh.php';
+
+class Product extends Dbh
+{
+    public function createProduct($categoryId, $name, $description, $image, $price, $stock)
+    {
+        $sql = "INSERT INTO products (category_id, product_name, description, product_image, price, stock_quantity) VALUES (:categoryId, :name, :description, :image, :price, :stock)";
+        $params = [
+            ':categoryId' => $categoryId,
+            ':name' => $name,
+            ':description' => $description,
+            ':image' => $image,
+            ':price' => $price,
+            ':stock' => $stock
+        ];
+        return $this->execute($sql, $params);
+    }
+
+    public function getAllProducts()
+    {
+        $sql = "SELECT product_id, category_id, product_name, description, product_image, price, stock_quantity FROM products";
+        return $this->fetchAll($sql);
+    }
+
+    public function getSingleProduct($id)
+    {
+        $sql = "SELECT product_id, category_id, product_name, description, product_image, price, stock_quantity FROM products WHERE product_id = :id";
+        $params = [':id' => $id];
+        return $this->fetch($sql, $params);
+    }
+}
+
+    
+
+// product_id, category_id, product_name, description, product_image, price, stock_quantity
