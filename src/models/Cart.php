8ce766b593d5f324d3105535667cb4ed85802623
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2024-08-22 16:32:44 +0000

    Implemented AddToCart functionality

--- src/models/Cart.php
+++ src/models/Cart.php
@@ -19,9 +19,22 @@ class Cart extends Dbh
         return $this->execute($sql, $params);
     }
 
+    public function updataCart($userId, $productId, $quantity)
+    {
+        $sql = "UPDATE cart SET user_id = :userID, product_id = :product_id, quantity = :quantity 
+        WHERE product_id = :product_id";
+        $params = [
+            ':userID' => $userId,
+            ':product_id' => $productId,
+            ':quantity' => $quantity,
+        ];
+
+        return $this->execute($sql, $params);
+    }
+
     public function getAllCarts()
     {
-        $sql = "SELECT cart.cart_id, cart.user_id, cart.product_id, cart.quantity, products.product_name, products.price, products.product_image 
+        $sql = "SELECT cart.cart_id, cart.user_id, cart.product_id, cart.quantity,      products.product_name, products.price, products.product_image 
                 FROM cart
                 JOIN products ON cart.product_id = products.product_id
                 JOIN users ON cart.user_id = users.user_id";
@@ -31,17 +44,29 @@ class Cart extends Dbh
     public function getSingleCart($id)
     {
         $sql = "SELECT cart.cart_id, users.user_id, products.product_id, cart.quantity, products.product_name, products.price, products.product_image 
-                FROM cart
-                JOIN ON cart.product_id = products.product_id
-                JOIN ON cart.user_id = users.user_id 
-                WHERE cart_id = :id";
+        FROM cart 
+        JOIN products ON cart.product_id = products.product_id 
+        JOIN users ON cart.user_id = users.user_id 
+        WHERE users.user_id = :id";
         $params = [':id' => $id];
         return $this->fetch($sql, $params);
     }
 
+    public function getUserCart($id)
+    {
+        $sql = "SELECT cart.cart_id, users.user_id, products.product_id, cart.quantity, products.product_name, products.price, products.product_image 
+                FROM cart 
+                JOIN products ON cart.product_id = products.product_id 
+                JOIN users ON cart.user_id = users.user_id 
+                WHERE users.user_id = :id";
+        $params = [':id' => $id];
+        return $this->fetchAll($sql, $params);
+    }
+
+
     public function deleteCart($id)
     {
-        $sql = "DELETE FROM cart WHERE cart_id = :id";
+        $sql = "DELETE FROM cart WHERE product_id = :id";
         $params = [':id' => $id];
         return $this->execute($sql, $params);
     }
